b495e9f559e3eb6814d848fee69f3a1fbd334f2f
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2015-02-13 14:31:20 +0100

    method definition types

--- lib/roman_number_kata.ex
+++ lib/roman_number_kata.ex
@@ -1,2 +1,15 @@
 defmodule RomanNumberKata do
+
+  def convert(number) when number < 1 do # methods with guard statement
+    ""
+  end
+
+  def convert(2) do # match arguments by value
+    "II"
+  end
+
+  def convert(number) do # regular method definition
+    "I"
+  end
+
 end
--- test/roman_number_kata_test.exs
+++ test/roman_number_kata_test.exs
@@ -1,7 +1,16 @@
 defmodule RomanNumberKataTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "it converts 0" do
+    assert RomanNumberKata.convert(0) == ""
   end
+
+  test "it convers 1" do
+    assert RomanNumberKata.convert(1) == "I"
+  end
+
+  test "it converts 2" do
+    assert RomanNumberKata.convert(2) == "II"
+  end
+
 end
