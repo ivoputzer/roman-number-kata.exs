4d248cef524690337057c60a87b46c117ad7b1ec
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2015-02-16 19:17:52 +0100

    making functions private

--- lib/roman_number_kata.ex
+++ lib/roman_number_kata.ex
@@ -4,15 +4,15 @@ defmodule RomanNumberKata do
     eval(num, [{10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}])
   end
 
-  def eval(num, _) when num < 1 do
+  defp eval(num, _) when num < 1 do
     ""
   end
 
-  def eval(num, [{arabic, roman} | tail]) when num >= arabic do
+  defp eval(num, [{arabic, roman} | tail]) when num >= arabic do
     roman <> eval(num - arabic, [{arabic, roman} | tail])
   end
 
-  def eval(num, [_ | tail]) do
+  defp eval(num, [_ | tail]) do
     eval(num, tail)
   end
 
