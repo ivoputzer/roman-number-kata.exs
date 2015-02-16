f43630f9b789907ea308b67beecb3c0f5d17c89e
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2015-02-16 18:28:39 +0100

    testing through iterators

--- test/roman_number_kata_test.exs
+++ test/roman_number_kata_test.exs
@@ -1,20 +1,11 @@
 defmodule RomanNumberKataTest do
   use ExUnit.Case
 
-  test "it converts 0" do
-    assert RomanNumberKata.convert(0) == ""
-  end
-
-  test "it convers 1" do
-    assert RomanNumberKata.convert(1) == "I"
-  end
-
-  test "it converts 2" do
-    assert RomanNumberKata.convert(2) == "II"
-  end
-
-  test "it converts 3" do
-    assert RomanNumberKata.convert(3) == "III"
+  test "converts arabic into roman number" do
+    test_values = [{0, ""}, {1, "I"}, {2, "II"}, {3, "III"}]
+    Enum.each test_values, fn({arabic, roman}) -> #pattern matching on anonymous function
+      assert RomanNumberKata.convert(arabic) == roman
+    end
   end
 
 end
